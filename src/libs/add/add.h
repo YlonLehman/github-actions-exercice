97d58d6396e8b1db96ba0f522d1eede24afb5a38
Némo Cazin <nemo.cazin@gmail.com>
2026-02-27 19:46:57 +0100

    feat: add header security

--- src/libs/add/add.h
+++ src/libs/add/add.h
@@ -3,7 +3,12 @@
  * @date       2026
  */
 
+#ifndef ADD_H
+#define ADD_H
+
 #include <stdio.h>
 
 // Prototype for the add function
 int add(int a, int b);
+
+#endif
--- src/libs/minus/minus.h
+++ src/libs/minus/minus.h
@@ -3,7 +3,12 @@
  * @date       2026
  */
 
+#ifndef MINUS_H
+#define MINUS_H
+
 #include <stdio.h>
 
 // Prototype for the minus function
 int minus(int a, int b);
+
+#endif
